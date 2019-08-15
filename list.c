054416c8cda06cefe671a55f19a10ee4a6682f44
Kei Nohguchi <kei@nohguchi.com>
2019-08-15 00:28:25 -0700

    list: Make list_ins_next() and list_rem_next() O(1)

--- list.c
+++ list.c
@@ -32,24 +32,15 @@ int list_ins_next(struct list *l, struct node *n, const void *data)
 {
 	struct node **nextp, *newp;
 
-	if (!n) {
-		nextp = &l->head;
-		goto insert;
-	}
-	for (nextp = &l->head; *nextp; nextp = &(*nextp)->next) {
-		if (*nextp == n) {
-			nextp = &(*nextp)->next;
-			goto insert;
-		}
-	}
-	errno = EINVAL;
-	return -1;
-insert:
 	newp = malloc(sizeof(struct node));
 	if (!newp)
 		return -1;
 	memset(newp, 0, sizeof(struct node));
 	newp->data = data;
+	if (!n)
+		nextp = &l->head;
+	else
+		nextp = &n->next;
 	if (*nextp)
 		newp->next = (*nextp)->next;
 	if (!newp->next)
@@ -61,24 +52,12 @@ insert:
 
 int list_rem_next(struct list *l, struct node *n, void **data)
 {
-	struct node *node, *prev = NULL;
+	struct node *node, *prev = n;
 
-	if (!n) {
+	if (n)
+		node = n->next;
+	else
 		node = l->head;
-		if (node == NULL) {
-			errno = EINVAL;
-			return -1;
-		}
-		goto remove;
-	}
-	for (node = l->head; node; node = node->next) {
-		if (node == n)
-			goto remove;
-		prev = node;
-	}
-	errno = EINVAL;
-	return -1;
-remove:
 	l->size -= 1;
 	*data = (void *)node->data;
 	if (prev)
