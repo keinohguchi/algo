b764a41dfab6c7c20987b741a47537a2a5c4f782
Kei Nohguchi <kei@nohguchi.com>
2019-10-12 15:20:20 -0700

    set: Add set_is_subset() unit test

--- set_test.c
+++ set_test.c
@@ -597,6 +597,211 @@ err:
 	return fail;
 }
 
+static int test_set_is_subset(void)
+{
+	const struct test {
+		const char	*const name;
+		int		size1;
+		int		size2;
+		int		data1[8];
+		int		data2[8];
+		int		want;
+	} *t, tests[] = {
+		{
+			.name		= "set_is_subset(): zero sets subset",
+			.size1		= 0,
+			.size2		= 0,
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 1 set each subset",
+			.size1		= 1,
+			.data1		= {1},
+			.size2		= 1,
+			.data2		= {2},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 2 set each subset",
+			.size1		= 2,
+			.data1		= {1, 2},
+			.size2		= 2,
+			.data2		= {3, 4},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 4 set each subset",
+			.size1		= 4,
+			.data1		= {1, 2, 3, 4},
+			.size2		= 4,
+			.data2		= {5, 6, 7, 8},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 8 set each subset",
+			.size1		= 8,
+			.data1		= {1, 2, 3, 4, 5, 6, 7, 8},
+			.size2		= 8,
+			.data2		= {15, 16, 17, 18, 19, 20, 21, 22},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 1 set duplicate subset",
+			.size1		= 1,
+			.data1		= {1},
+			.size2		= 1,
+			.data2		= {1},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 2 set duplicate subset",
+			.size1		= 2,
+			.data1		= {1, 2},
+			.size2		= 2,
+			.data2		= {2, 1},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 4 set duplicate subset",
+			.size1		= 4,
+			.data1		= {1, 2, 3, 4},
+			.size2		= 4,
+			.data2		= {3, 4, 1, 2},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 8 set duplicate subset",
+			.size1		= 8,
+			.data1		= {1, 2, 3, 4, 5, 6, 7, 8},
+			.size2		= 8,
+			.data2		= {5, 6, 7, 8, 4, 3, 2, 1},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 1 and 2 set subset",
+			.size1		= 1,
+			.data1		= {1},
+			.size2		= 2,
+			.data2		= {2, 1},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 2 and 4 set subset",
+			.size1		= 2,
+			.data1		= {1, 2},
+			.size2		= 4,
+			.data2		= {2, 3, 4, 1},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 4 and 8 set subset",
+			.size1		= 4,
+			.data1		= {1, 2, 3, 4},
+			.size2		= 8,
+			.data2		= {3, 4, 1, 9, 8, 7, 6, 2},
+			.want		= 1,
+		},
+		{
+			.name		= "set_is_subset(): 1 and 2 set non subset",
+			.size1		= 1,
+			.data1		= {1},
+			.size2		= 2,
+			.data2		= {2, 3},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 2 and 4 set non subset",
+			.size1		= 2,
+			.data1		= {1, 2},
+			.size2		= 4,
+			.data2		= {2, 3, 4, 0},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 4 and 8 set non subset",
+			.size1		= 4,
+			.data1		= {1, 2, 3, 4},
+			.size2		= 8,
+			.data2		= {3, 4, 1, 9, 8, 7, 6, 0},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 2 and 1 set non subset",
+			.size1		= 2,
+			.data1		= {2, 3},
+			.size2		= 1,
+			.data2		= {1},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 4 and 2 set non subset",
+			.size1		= 4,
+			.data1		= {2, 3, 4, 0},
+			.size2		= 2,
+			.data2		= {1, 2},
+			.want		= 0,
+		},
+		{
+			.name		= "set_is_subset(): 8 and 4 set non subset",
+			.size1		= 8,
+			.data1		= {3, 4, 1, 9, 8, 7, 6, 0},
+			.size2		= 4,
+			.data2		= {1, 2, 3, 4},
+			.want		= 0,
+		},
+		{.name = NULL},
+	};
+	int fail = 0;
+
+	for (t = tests; t->name; t++) {
+		struct set s1, s2;
+		int i, ret, got;
+
+		ret = set_init(&s1, same, NULL);
+		if (ret == -1)
+			goto perr;
+		ret = set_init(&s2, same, NULL);
+		if (ret == -1)
+			goto perr;
+		for (i = 0; i < t->size1; i++) {
+			ret = set_insert(&s1, &t->data1[i]);
+			if (ret == -1)
+				goto perr;
+		}
+		if (set_size(&s1) != t->size1) {
+			fprintf(stderr, "%s: unexpected set1 size:\n\t- want: %d\n\t-  got: %d\n",
+				t->name, t->size1, set_size(&s1));
+			goto err;
+		}
+		for (i = 0; i < t->size2; i++) {
+			ret = set_insert(&s2, &t->data2[i]);
+			if (ret == -1)
+				goto perr;
+		}
+		if (set_size(&s2) != t->size2) {
+			fprintf(stderr, "%s: unexpected set2 size:\n\t- want: %d\n\t-  got: %d\n",
+				t->name, t->size2, set_size(&s2));
+			goto err;
+		}
+		got = set_is_subset(&s1, &s2);
+		if (got != t->want) {
+			fprintf(stderr, "%s: unexpected subset:\n\t- want: %d\n\t-  got: %d\n",
+				t->name, t->want, got);
+			goto err;
+		}
+		set_destroy(&s2);
+		set_destroy(&s1);
+		continue;
+perr:
+		perror(t->name);
+err:
+		set_destroy(&s2);
+		set_destroy(&s1);
+		fail++;
+	}
+	return fail;
+}
+
 static int test_set_is_equal(void)
 {
 	const struct test {
@@ -763,6 +968,7 @@ int main(void)
 	fail += test_set_union();
 	fail += test_set_intersect();
 	fail += test_set_difference();
+	fail += test_set_is_subset();
 	fail += test_set_is_equal();
 	if (fail)
 		exit(EXIT_FAILURE);
